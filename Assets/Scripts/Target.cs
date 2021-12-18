0ab2d3426cf77f57f0e385db4a38034f3777c107
Jordan Davis <jordandavisporter@Hotmail.com>
2021-12-18 13:51:40 -0400

    Initial commit

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour
+{
+    public float health = 100f;
+    public SpriteRenderer m_spriteRenderer;
+    //this script goes on the enemy along with a box collider
+    //Tobias, a note about Sprite Renderers. They are gameobjects in themselves. You previously change this script to
+    //create a second gameobject variable to call the render false in the death, no need to convulute the code if we can just
+    //call the render false on it's own.
+    //Please check with me before making changes to the sprite functions. 
+    public void Start()
+    {
+        m_spriteRenderer = GetComponent<SpriteRenderer>(); //basically instantiating the color pallette in the scene
+    }
+    public void takeDamage(float amount)
+    {
+        StartCoroutine(DamageEffect()); //loops the color change sequence upon taking damage
+        health -= amount;
+
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+    IEnumerator DamageEffect()
+    {
+        m_spriteRenderer.color = Color.red;
+
+        yield return new WaitForSeconds(0.05f); //timer changing the color back to white/default
+
+        m_spriteRenderer.color = Color.white;
+    }
+
+    void Die()
+    {
+        gameObject.SetActive(false); 
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MackFreemaker
+ The rail shooter I conceptualized and developed with a team from my game development course
